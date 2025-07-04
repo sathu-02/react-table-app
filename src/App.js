422727ff9b34eb4921ec572ccdaea857f74f16f4
sathu-02 <156219201+sathu-02@users.noreply.github.com>
2025-07-04 11:36:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const App = () => {
     { id: 3, name: "Charlie", age: 25, salary: 48000 },
     { id: 4, name: "Diana", age: 29, salary: 59000 },
     { id: 5, name: "Ethan", age: 35, salary: 70000 },
+    { id: 6, name: "Chethan", age: 19, salary: 100000 },
+    { id: 7, name: "Rithwik", age: 20, salary: 600000 },
+    { id: 8, name: "Avaneesh", age: 20, salary: 400000 },
+    { id: 9, name: "Satwik", age: 21, salary: 500000 },
+    { id: 10, name: "Harshit", age: 22, salary: 7000000 },
   ];
 
   return (
